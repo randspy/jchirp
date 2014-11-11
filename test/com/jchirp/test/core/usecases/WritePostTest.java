b2949c2d717ae279da0383208326b7d58b3ed815
Przemyslaw Koziel <mos.koziel@gmail.com>
2014-11-11 14:05:14 +0100

    Second test added with some test refactoring.

--- test/com/jchirp/test/core/usecases/WritePostTest.java
+++ test/com/jchirp/test/core/usecases/WritePostTest.java
@@ -6,25 +6,50 @@ import com.jchirp.core.persistence.Context;
 import com.jchirp.core.usecases.Command;
 import com.jchirp.core.usecases.WritePost;
 import com.jchirp.test.core.usecases.persistence.MockGateway;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class WritePostTest {
 
-    public static final String USER = "user";
-    public static final String CONTENT = "content";
+    private static final String USER = "user";
+    private static final String POST = "post";
+    private Command usecase;
+
+    private void executeRequest(String user, String content) {
+        usecase.execute(new Request(user, content));
+    }
+
+    private void assertUser(String userName, String content, int postIndex) {
+        User user = Context.gateway.getUser(userName);
+        assertEquals(userName, user.getUserName());
+        assertEquals(content, user.getPosts().get(postIndex).getContent());
+    }
+
+    @Before
+    public void setUp(){
+        usecase = new WritePost();
+        Context.gateway = new MockGateway();
+    }
 
     @Test
     public void user_is_added_if_does_not_exist_already(){
-        Command usecase = new WritePost();
-        Context.gateway = new MockGateway();
 
-        Request request = new Request(USER, CONTENT);
+        executeRequest(USER, POST);
 
-        usecase.execute(request);
-        User user = Context.gateway.getUser(USER);
-        assertEquals(USER, user.getUserName());
-        assertEquals(CONTENT, user.getPosts().get(0).getContent());
+        int postIndex = 0;
+        assertUser(USER, POST, postIndex);
+    }
+
+    @Test
+    public void new_post_is_added_to_user(){
+        String newPost = "new post";
+
+        executeRequest(USER, POST);
+        executeRequest(USER, newPost);
+
+        int newPostIndex = 1;
+        assertUser(USER, newPost, newPostIndex);
     }
 }
