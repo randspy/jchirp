48894aee5b855a899b884a85ef5bfb737a483949
Przemyslaw Koziel <mos.koziel@gmail.com>
2014-12-06 16:22:13 +0100

    Refactoring.

--- src/com/jchirp/core/messages/PostMsg.java
+++ src/com/jchirp/core/messages/PostMsg.java
@@ -3,14 +3,47 @@ package com.jchirp.core.messages;
 import org.joda.time.DateTime;
 
 public class PostMsg implements Comparable<PostMsg>{
-    private final String userName;
-    private final String content;
-    private final DateTime timestamp;
+    private String userName = "";
+    private String content = "";
+    private DateTime timestamp = new DateTime();
+    private DateTime currentTime = new DateTime();
 
-    public PostMsg(String userName, String content, DateTime timestamp) {
-        this.userName = userName;
-        this.content = content;
-        this.timestamp = timestamp;
+    public static class Builder{
+        private String userName;
+        private String content;
+        private DateTime timestamp;
+        private DateTime currentTime;
+
+        public Builder withUserName(String userName) {
+            this.userName = userName;
+            return this;
+        }
+
+        public Builder withContent(String content) {
+            this.content = content;
+            return this;
+        }
+
+        public Builder withTimestamp(DateTime timestamp) {
+            this.timestamp = timestamp;
+            return this;
+        }
+
+        public Builder withCurrentTime(DateTime currentTime) {
+            this.currentTime = currentTime;
+            return this;
+        }
+
+        public PostMsg build() {
+            return new PostMsg(this);
+        }
+    }
+
+    public PostMsg(Builder post) {
+        userName = post.userName;
+        content = post.content;
+        timestamp = post.timestamp;
+        currentTime = post.currentTime;
     }
 
     public String getUserName() {
@@ -19,7 +52,11 @@ public class PostMsg implements Comparable<PostMsg>{
 
     public String getContent() { return content; }
 
-    public DateTime getTimestamp() { return timestamp; }
+    public DateTime getPostTimestamp() { return timestamp; }
+
+    public DateTime getCurrentTime() {
+        return currentTime;
+    }
 
     @Override
     public int compareTo(PostMsg postMsg) {
